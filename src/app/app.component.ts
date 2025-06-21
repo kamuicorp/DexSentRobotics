2e69802d7df81279ce9097152c80766323518623
Yashwanthkumar Arivazhagan <yashwanthkumar.arivazhagan@tetherfi.com>
2025-06-21 09:17:51 +0530

    lander1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { LanderComponent } from './lander/lander.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [LanderComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'DexSentRobotics';
 }
---
+++ src/app/lander/lander.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-lander',
+  standalone: true,
+  imports: [],
+  templateUrl: './lander.component.html',
+  styleUrl: './lander.component.scss'
+})
+export class LanderComponent {
+
+}
